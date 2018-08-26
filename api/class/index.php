5215369b26423fe67064b894c21b9f6e3a36b98d
Nguyen Tien Dat <datnt.it.pro@gmail.com>
2018-08-26 18:59:10 +0700

    [class] update get api

--- api/class/index.php
+++ api/class/index.php
@@ -1,33 +1,33 @@
-<?php 
+<?php
 // Require the person class file
   require_once("class.dao.php");
   require_once(__DIR__ . "/../../helper/http-request.php");
-  require_once(__DIR__ . "/../../helper/utility.php");
+  require_once(__DIR__ . "/../../Db.class.php");
 	
 // Instantiate the person class
   $classDAO  = new ClassDAO();
   $http_request = new http_request();
   $body = json_decode($http_request->body());
-  $utility = new Utility();
+  $db = new DB();
 
   switch ($_SERVER['REQUEST_METHOD']) {
     case 'POST': // add
       if (isset($_REQUEST['id'])) {
         $classDAO->id = $_REQUEST['id'];
+        $classDAO->course_id_ref = $body->course_id_ref;
         $classDAO->name = $body->name;
         $classDAO->start_date = $body->start_date;
         $classDAO->end_date = $body->end_date;
         $classDAO->description = $body->description;
-        $classDAO->course_id_ref = $body->course_id_ref;
         $save = $classDAO->save();
         $http_request->sendJsonResponse('success', 200, $save);
         die();
       } else {
         $classDAO->name = $body->name;
+        $classDAO->course_id_ref = $body->course_id_ref;
         $classDAO->start_date = $body->start_date;
         $classDAO->end_date = $body->end_date;
         $classDAO->description = $body->description;
-        $classDAO->course_id_ref = $body->course_id_ref;
         $create = $classDAO->create();
         $http_request->sendJsonResponse('success', 200, $create);
         die();
@@ -48,7 +48,8 @@
         $http_request->sendJsonResponse('success', 200, $classDAO->variables);
         die();
       } else {
-        $getAll = $classDAO->all();
+        $queryGetString = 'SELECT class.*, course.name as "course_name", course.description as "course_des", course.cost as "course_cost" FROM class LEFT JOIN course ON class.course_id_ref = course.id';
+        $getAll = $db->query($queryGetString);
         $http_request->sendJsonResponse('success', 200, $getAll);
         die();
       }
@@ -58,10 +59,10 @@
     case 'PUT': // edit
       if (isset($_REQUEST['id'])) {
         $classDAO->id = $_REQUEST['id'];
+        $classDAO->course_id_ref = $body->course_id_ref;
         $classDAO->name = $body->name;
         $classDAO->start_date = $body->start_date;
         $classDAO->end_date = $body->end_date;
-        $classDAO->course_id_ref = $body->course_id_ref;
         $classDAO->description = $body->description;
         $save = $classDAO->save();
         $http_request->sendJsonResponse('success', 200, $save);
